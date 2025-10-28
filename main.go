2cf056b5c0fa07289ea8baf091c7c681960c31d5
Soham Maity <sohammaity006@gmail.com>
2025-10-28 21:20:07 +0530

    feat: implement terminal resizing and rendering updates

--- main.go
+++ main.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/yourusername/wtop/ui"
+	"github.com/SwarnenduG07/wtop/ui"
 )
 
 func main() {
 	ui.HideCursor()
 	defer ui.ShowCursor()
-	
+
 	fmt.Printf("%sStarting wtop... Press Ctrl+C to exit%s\n", ui.Green, ui.Reset)
 	time.Sleep(1 * time.Second)
-	
+
 	ui.ClearScreen()
-	
+
+	resizeCh := make(chan os.Signal, 1)
+	signal.Notify(resizeCh, syscall.SIGWINCH)
+
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
+
+	render := func() {
+		width, height := ui.GetTerminalSize()
+		ui.RenderFrame(width, height)
+	}
+
+	render()
+
 	for {
-		row := 1
-		
-		ui.RenderHeader(&row)
-		ui.RenderCPU(&row)
-		ui.RenderMemory(&row)
-		ui.RenderGPU(&row)
-		ui.RenderSystemInfo(&row)
-		ui.RenderProcessTable(&row)
-		ui.RenderFooter(&row)
-		
-		time.Sleep(2 * time.Second)
+		select {
+		case <-ticker.C:
+			render()
+		case <-resizeCh:
+			ui.ClearScreen()
+			render()
+		}
 	}
 }
