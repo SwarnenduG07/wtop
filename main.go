b1127841ff22c35b1ab3e0395d242358b175ce4e
Soham Maity <sohammaity006@gmail.com>
2025-10-29 23:00:53 +0530

    refactor: simplify main function by removing cursor handling and rendering logic

--- main.go
+++ main.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
 
 	"github.com/SwarnenduG07/wtop/ui"
 )
 
 func main() {
-	ui.HideCursor()
-	defer ui.ShowCursor()
-
-	fmt.Printf("%sStarting wtop... Press Ctrl+C to exit%s\n", ui.Green, ui.Reset)
-	time.Sleep(1 * time.Second)
-
-	ui.ClearScreen()
-
-	resizeCh := setupResizeCh()
-
-	ticker := time.NewTicker(2 * time.Second)
-	defer ticker.Stop()
-
-	render := func() {
-		width, height := ui.GetTerminalSize()
-		ui.RenderFrame(width, height)
-	}
-
-	render()
-
-	for {
-		select {
-		case <-ticker.C:
-			render()
-		case <-resizeCh:
-			ui.ClearScreen()
-			render()
-		}
+	dashboard := ui.NewDashboard(ui.DefaultTheme())
+	if err := dashboard.Run(); err != nil {
+		log.Fatalf("wtop: %v", err)
 	}
 }
